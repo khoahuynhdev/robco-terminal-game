8e72c734ab4639b76e2e94b5408ad7938982fc8c
khoahuynhdev <khoa0319@gmail.com>
2020-11-24 12:04:19 +0700

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Main component</h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+
+});
+</script>
+ <style lang="sass" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <Main />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Main from '@/components/Main.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
-    HelloWorld,
+    Main,
   },
 });
 </script>
